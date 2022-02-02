4eed9aec5044ae06cce56bf600d8818434e054d0
wcthomason <wcthomason@bitbucket.org>
2022-02-02 10:44:21 -0600

    Added welcome message

--- Program.cs
+++ Program.cs
@@ -12,6 +12,11 @@ namespace examplerepo
 
             string answerStr;
 
+            Console.WriteLine("\n\n---------------Welcome to 2 Number Multiplication!!---------------\n");
+            Console.WriteLine("All I can do for you is multiply one number by a second number...");
+            Console.WriteLine("Don't know how to put commas in your answer either so sorry about that...\n");
+            Console.WriteLine("Have fun though! Multiply as many combinations of 2 numbers as your heart desires\n\n");
+
             Console.WriteLine("Enter value 1: ");
             num1 = double.Parse(Console.ReadLine());
 
