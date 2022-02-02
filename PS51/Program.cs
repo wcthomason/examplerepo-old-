b12e8b409af7b8104fa749716940e552075173ce
wcthomason <wcthomason@bitbucket.org>
2022-02-02 16:07:00 -0600

    Added Psalm 51:1

---
+++ PS51/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PS51
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\n\n-----------Psalm 51:1-----------");
+            Console.WriteLine("Have mercy on me, O God\n    according to your steadfast love;\n    according to your abundant mercy\n blot out my transgressions.");
+            
+        }
+    }
+}
