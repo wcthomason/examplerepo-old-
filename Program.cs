dd474aa6b6916d1e8f81dcd4f33bf88eec691e43
wcthomason <wcthomason@bitbucket.org>
2022-02-02 10:32:31 -0600

    Changed program to Calculator

--- Program.cs
+++ Program.cs
@@ -4,9 +4,25 @@ namespace examplerepo
 {
     class Program
     {
-        static void Main(string[] args)
+         static void Main(string[] args)
         {
-            Console.WriteLine("Git is great!");
+            double num1 = 0;
+            double num2 = 0;
+            double answer = 0;
+
+            string answerStr;
+
+            Console.WriteLine("Enter value 1: ");
+            num1 = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter value 2: ");
+            num2 = double.Parse(Console.ReadLine());
+
+            answer = num1*num2;
+            answerStr = answer.ToString();
+
+            Console.WriteLine("Answer is: " + answer);
+
         }
     }
 }
